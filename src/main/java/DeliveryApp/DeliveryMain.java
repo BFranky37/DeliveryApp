d067a162c1ede9a9a05a35af2dd545beb7e1ffa1
BFranky37 <bafrankl@my.loyno.edu>
2022-09-11 14:36:38 -0500

    Project structure set up

---
+++ src/main/java/DeliveryApp/DeliveryMain.java
@@ -0,0 +1,12 @@
+package DeliveryApp;
+
+import org.apache.log4j.Logger;
+
+public class DeliveryMain {
+    private static final Logger LOGGER = Logger.getLogger(DeliveryMain.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!");
+
+    }
+}
