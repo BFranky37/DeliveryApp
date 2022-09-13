1cb9ae512be7409fc6660c317e3adddf3d82f053
BFranky37 <bafrankl@my.loyno.edu>
2022-09-13 16:13:56 -0500

    read from db.properties file

--- src/main/java/deliveryapp/utils/ConnectionPool.java
+++ src/main/java/deliveryapp/utils/ConnectionPool.java
@@ -3,24 +3,51 @@ package deliveryapp.utils;
 import deliveryapp.utils.exceptions.ConnectionException;
 import org.apache.log4j.Logger;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class ConnectionPool {
     private static final Logger LOGGER = Logger.getLogger(ConnectionPool.class.getName());
 
     private static ConnectionPool pool;
-    private List<Connection> connectionPool = new ArrayList<>(5);
+    private final List<Connection> connectionPool = new ArrayList<>(5);
 
     private ConnectionPool() {
-        for (int i = 0; i < 5; i++) {
+        Properties props = new Properties(); //Read JDBC driver, url, db name, and password from db.properties file
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream("src/main/resources/db.properties");
+            props.load(in);
+            in.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage() + ", could not read db properties file");
+        }
+
+        try { //Load JDBC driver
+            String driver = props.getProperty("jdbc.driver");
+            if (driver != null) {
+                Class.forName(driver) ;
+            }
+        } catch (ClassNotFoundException e) {
+            LOGGER.error(e.getMessage() + ", could not load driver");
+        }
+
+        String url = props.getProperty("jdbc.url");
+        String username = props.getProperty("jdbc.username");
+        String password = props.getProperty("jdbc.password");
+
+        for (int i = 0; i < 5; i++) { //Add connections to pool
             try {
-                connectionPool.add(DriverManager.getConnection("jdbc:mysql://localhost:3306/DeliveryApp", "root", "al48fh6!!boo"));
+                connectionPool.add(DriverManager.getConnection(url, username, password));
             } catch (SQLException e) {
-                LOGGER.error(e.getMessage());
+                LOGGER.error(e.getMessage() + "could not add connection to pool");
             }
         }
     }
