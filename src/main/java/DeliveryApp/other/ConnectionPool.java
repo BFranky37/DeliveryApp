99631fb78267f30a1067db89247af56ba30ac5d6
BFranky37 <bafrankl@my.loyno.edu>
2022-09-13 13:23:58 -0500

    Fixed ConnectionPool

--- src/main/java/DeliveryApp/other/ConnectionPool.java
+++ src/main/java/DeliveryApp/other/ConnectionPool.java
@@ -1,16 +1,28 @@
 package DeliveryApp.other;
 
+import DeliveryApp.people.Address;
+import org.apache.log4j.Logger;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionPool {
+    private static final Logger LOGGER = Logger.getLogger(ConnectionPool.class.getName());
 
     private static ConnectionPool pool;
     private List<Connection> connectionPool = new ArrayList<>(5);
 
     private ConnectionPool() {
-
+        for (int i = 0; i < 5; i++) {
+            try {
+                connectionPool.add(DriverManager.getConnection("jdbc:mysql://localhost:3306/DeliveryApp", "root", "al48fh6!!boo"));
+            } catch (SQLException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
     }
 
     public static ConnectionPool getInstance() {
